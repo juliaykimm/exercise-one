d7b74aa61d6fbde756d0cf41665762aff030fe4f
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-09-15 15:29:37 -0400

    Adding new stuff

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,9 +1,9 @@
 import React from "react"; // tells the file we are using React
 
-function ArticleCard() {
+function ArticleCard({ articleData }) {
   return (
     <div>
-      <h2> Article Title</h2>
+      <h2> {articleData.title}</h2>
     </div>
   );
 }
--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,14 +1,20 @@
 import React from "react"; // tells the file we are using React
 // Component imports
 import ArticleCard from "../components/ArticleCard";
+// Data imports
+import Data from "../components/data";
 
 function ArticleListing() {
+  // props send data through the flow of components
   return (
-    <section>
+    <main>
       <header>
-        <h1>Articles </h1>
+        <h1>Articles</h1>
       </header>
-    </section>
+      {Data.map((article, i) => (
+        <ArticleCard key={i} articleData={article} />
+      ))}
+    </main>
   );
 }
 

49780e9618c0ea5f11e2c1b45f1af4e5bef4eeba
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-09-17 15:32:30 -0400

    layed our article listing and implement react router

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,9 +1,19 @@
 import React from "react"; // tells the file we are using React
 
+import { Link } from "react-router-dom";
+
 function ArticleCard({ articleData }) {
   return (
-    <div>
-      <h2> {articleData.title}</h2>
+    <div className="articleCard">
+      <div className="articleCardImage">
+        <img src={articleData.image.url} alt={articleData.image.alt}></img>
+      </div>
+      <div className="articleCardText">
+        <h2> {articleData.title} </h2>
+        <p>{articleData.publishedDate}</p>
+        <p>{articleData.blurb}</p>
+        <Link to={`/article/${articleData.id}`}>Read More</Link>
+      </div>
     </div>
   );
 }
--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
 function Article() {
+  let { id } = useParams();
+
+  console.log("id", id);
   return (
     <section>
       <h1>Article</h1>
+      <p>{id}</p>
     </section>
   );
 }

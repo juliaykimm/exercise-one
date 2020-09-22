2965a04659df15ab65c3d9b6df5cd87cd41aee25
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-09-22 14:21:35 -0400

    added dates/fixed

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,7 +2,41 @@ import React from "react"; // tells the file we are using React
 
 import { Link } from "react-router-dom";
 
+const days = [
+  "Sunday", // 0
+  "Monday", // 1
+  "Tuesday", // 2
+  "Wednesday", // 3
+  "Thursday", // ...
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 function ArticleCard({ articleData }) {
+  const date = new Date(articleData.publishedDate);
+  const dayOfWeek = date.getDay();
+  const calendarDate = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const day = days[dayOfWeek];
+
   return (
     <div className="articleCard">
       <div className="articleCardImage">
@@ -10,7 +44,7 @@ function ArticleCard({ articleData }) {
       </div>
       <div className="articleCardText">
         <h2> {articleData.title} </h2>
-        <p>{articleData.publishedDate}</p>
+        <p>{`${days[dayOfWeek]} ${months[month]} ${calendarDate}, ${year}`}</p>
         <p>{articleData.blurb}</p>
         <Link to={`/article/${articleData.id}`}>Read More</Link>
       </div>

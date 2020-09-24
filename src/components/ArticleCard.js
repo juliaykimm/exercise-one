cdfa9498f65c0f5889a845e76ebe0aa12650c418
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-09-23 21:35:07 -0400

    final exercise one

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -19,7 +19,7 @@ function ArticleCard({ articleData }) {
         <img src={articleData.image.url} alt={articleData.image.alt}></img>
       </div>
       <div className="ArticleCardText">
-        <h2> {articleData.title} </h2>
+        <h2 className="ArticleTitle"> {articleData.title} </h2>
         <p>{`${days[dayOfWeek]} ${months[month]} ${calendarDate}, ${year}`}</p>
         <p>{articleData.blurb}</p>
         <Link to={`/article/${articleData.id}`}>Read More</Link>
